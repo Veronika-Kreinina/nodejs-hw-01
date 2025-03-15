b4fc0d3ed705e8fefd7d467145e988b2c934a1bf
Veronika Kreinina <veronika.kr.0608@gmail.com>
2025-03-15 21:34:13 +0100

    add scripts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,14 @@
+import { readFile } from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const data = await readFile(PATH_DB, 'utf-8');
+  return JSON.parse(data);
+};
+
+//   try {
+//     const contacts = JSON.parse(data);
+//     return Array.isArray(contacts) ? contacts : [];
+//   } catch {
+//     return [];
+//   }
